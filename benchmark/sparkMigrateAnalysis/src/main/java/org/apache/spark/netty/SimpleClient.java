73ee7a1212212f944313c5c27d5ecaec3a0066fb
10564 <1056488772@qq.com>
2018-07-26 16:12:28 +0800

    add netty evaluation

---
+++ benchmark/sparkMigrateAnalysis/src/main/java/org/apache/spark/netty/SimpleClient.java
@@ -0,0 +1,51 @@
+package org.apache.spark.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+
+/*
+ *
+ * 执行jar包
+ * java -cp sparkMigrateAnalysis-2.3.3-SNAPSHOT.jar org.apache.spark.netty.SimpleClient
+ * */
+public class SimpleClient {
+
+    public void connect(String host, int port) throws Exception {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new SimpleClientHandler());
+                }
+            });
+
+            // Start the client.
+            ChannelFuture f = b.connect(host, port).sync();
+
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        SimpleClient client=new SimpleClient();
+//        client.connect("192.168.223.202", 19999);
+        client.connect("localhost", 19999);
+    }
+
+}
